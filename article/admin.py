dc1dd4ad820585804297e244edfbaaf5fa937a51
Ekin Söğüt <ekinsogut@users.noreply.github.com>
2020-09-09 14:44:15 +0300

    First Commit

---
+++ article/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from .models import Article,Comment,Rating
+
+# Register your models here.
+
+admin.site.register(Comment)
+admin.site.register(Rating)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+
+    list_display = ["title","author","created_date"]
+
+    list_display_links = ["title","created_date"]
+
+    search_fields = ["title"]
+
+    list_filter = ["created_date"]
+    class Meta:
+        model = Article
+
